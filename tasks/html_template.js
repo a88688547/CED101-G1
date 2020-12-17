67eb45e0387726a7ce3af95c8d604bea4214b00c
a88688547 <a88688547@gmail.com>
2020-12-17 13:44:38 +0800

    修改老師框架

--- tasks/html_template.js
+++ tasks/html_template.js
@@ -1,17 +1,17 @@
-const {
-    src,
-    dest
-} = require('gulp');
+const { src, dest } = require('gulp')
 
-const fileinclude = require('gulp-file-include');
+const fileinclude = require('gulp-file-include')
 
 //路徑參數
 const config = require('../gulp.config')
 
 module.exports = function htmlTemplate() {
     return src(config.paths.html) //input
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file'
-        })).pipe(dest(config.pathsDev.dist)) //output
+        .pipe(
+            fileinclude({
+                prefix: '@@',
+                basepath: '@file',
+            })
+        )
+        .pipe(dest(config.pathsDev.dist)) //output
 }

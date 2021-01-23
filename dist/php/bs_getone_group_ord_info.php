166b1cb0271689b9de0cbf261c85d403337f816f
a88688547 <a88688547@gmail.com>
2021-01-23 14:46:16 +0800

    後台 揪團訂單 php 修改

--- dist/php/bs_getone_group_ord_info.php
+++ dist/php/bs_getone_group_ord_info.php
@@ -22,6 +22,36 @@ try {
         //取回一筆資料
         $grouporddatarow = $grouporddata->fetch(PDO::FETCH_ASSOC);
 
+        // 判斷該訂單內 有沒有選擇 優惠券使用
+        if ($grouporddatarow["cou_no"] === null) {
+
+            $grouporddatarow["cou_text"] = "沒有使用";
+
+        } else {
+            //有選擇優惠券 就去 join table
+            $sql = "select *
+            from personal_order p
+            join member m on p.mem_no = m.mem_no
+            join coupon c on p.cou_no = c.cou_no
+            where p.per_ord_no = :per_ord_no";
+            $memberdata = $pdo->prepare($sql);
+            $memberdata->bindValue(":per_ord_no", $per_ord_no);
+            $memberdata->execute();
+            $grouporddatarow = $memberdata->fetch(PDO::FETCH_ASSOC);
+
+            if ($grouporddatarow["cou_discount"] == 0.9) {
+                $grouporddatarow["cou_text"] = "九折優惠";
+            } else if ($grouporddatarow["cou_discount"] == 0.8) {
+                $grouporddatarow["cou_text"] = "八折優惠";
+            } else if ($grouporddatarow["cou_discount"] == 0.7) {
+                $grouporddatarow["cou_text"] = "七折優惠";
+            } else if ($grouporddatarow["cou_discount"] == 0.6) {
+                $grouporddatarow["cou_text"] = "六折優惠";
+            }
+            ;
+
+        }
+
         //送出json字串
         echo json_encode($grouporddatarow);
         // echo $managerdatarow;
